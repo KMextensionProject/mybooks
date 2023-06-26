6e7eb8e8cd45fa11f2c2c0f4d90f97247c709d62
Martin Krajcovic <mar.krajc@gmail.com>
2023-06-26 19:14:45 +0200

    deleted ContentType enum since spring has one available

--- src/main/java/com/mkrajcovic/mybooks/enums/ContentType.java
+++
@@ -1,8 +0,0 @@
-package com.mkrajcovic.mybooks.enums;
-
-public class ContentType {
-
-	public static final String APPLICATION_JSON = "application/json";
-	public static final String MULTIPART_FORM_DATA = "multipart/form-data";
-
-}
